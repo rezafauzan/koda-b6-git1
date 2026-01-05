7b24fa93db538d607158065a6407bcab3855cfd2
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-05 21:34:50 +0700

    feat: add module to convert from celcius to fahrenheit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import {celciusToReamur} from "./my_modules/temp_converter/celcius-to-reamur.js"
 import {celciusToKelvin} from "./my_modules/temp_converter/celcius-to-kelvin.js"
+import {celciusToFahrenheit} from "./my_modules/temp_converter/celcius-to-fahrenheit.js"
 
 let temp = 40
 
 console.log(`Hasil konversi suhu ${temp}°Celcius ke "Reamur" adalah ${celciusToReamur(temp)}°Reamur`)
 console.log(`Hasil konversi suhu ${temp}°Celcius ke "Kelvin" adalah ${celciusToKelvin(temp)}°Kelvin`)
+console.log(`Hasil konversi suhu ${temp}°Celcius ke "Fahrenheit" adalah ${celciusToFahrenheit(temp)}°Fahrenheit`)
---
+++ my_modules/temp_converter/celcius-to-fahrenheit.js
@@ -0,0 +1,16 @@
+function celciusToFahrenheit(temp) {
+    try {
+        if (typeof temp === 'number') {
+            return temp = temp * 9 / 5 + 32
+        } else {
+            const error = new Error(`${typeof temp} ${temp} bukan sebuah angka`)
+            throw error
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {
+    celciusToFahrenheit
+}
