ee60b044ffda33132298514fc59194095870f53d
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-05 22:01:53 +0700

    feat: add interactivity now it can input temp in celcius

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ import {celciusToReamur} from "./my_modules/temp_converter/celcius-to-reamur.js"
 import {celciusToKelvin} from "./my_modules/temp_converter/celcius-to-kelvin.js"
 import {celciusToFahrenheit} from "./my_modules/temp_converter/celcius-to-fahrenheit.js"
 
-let temp = 40
-
-console.log(`Hasil konversi suhu ${temp}°Celcius ke "Reamur" adalah ${celciusToReamur(temp)}°Reamur`)
-console.log(`Hasil konversi suhu ${temp}°Celcius ke "Kelvin" adalah ${celciusToKelvin(temp)}°Kelvin`)
-console.log(`Hasil konversi suhu ${temp}°Celcius ke "Fahrenheit" adalah ${celciusToFahrenheit(temp)}°Fahrenheit`)
+console.log("Masukan angka suhu dalam celcius :")
+process.stdin.on("data",
+    input => {
+        try {
+            let temp = parseInt(input.toString().trim())
+            if (typeof temp === 'number') {     
+                console.log(`Hasil konversi suhu ${temp}°Celcius ke "Reamur" adalah ${celciusToReamur(temp)}°Reamur`)
+                console.log(`Hasil konversi suhu ${temp}°Celcius ke "Kelvin" adalah ${celciusToKelvin(temp)}°Kelvin`)
+                console.log(`Hasil konversi suhu ${temp}°Celcius ke "Fahrenheit" adalah ${celciusToFahrenheit(temp)}°Fahrenheit`)
+            }else{
+                let error = new Error(`${typeof temp} ${temp} bukanlah sebuah angka`)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+)
