dde478bf538f615d73363754243cc14f50d62f10
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-05 21:32:27 +0700

    feat: add module to convert from celcius to kelvin

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import {celciusToReamur} from "./my_modules/temp_converter/celcius-to-reamur.js"
+import {celciusToKelvin} from "./my_modules/temp_converter/celcius-to-kelvin.js"
 
 let temp = 40
 
 console.log(`Hasil konversi suhu ${temp}°Celcius ke "Reamur" adalah ${celciusToReamur(temp)}°Reamur`)
+console.log(`Hasil konversi suhu ${temp}°Celcius ke "Kelvin" adalah ${celciusToKelvin(temp)}°Kelvin`)
---
+++ my_modules/temp_converter/celcius-to-kelvin.js
@@ -0,0 +1,16 @@
+function celciusToKelvin(temp) {
+    try {
+        if (typeof temp === 'number') {
+            return temp = temp + 273.15
+        } else {
+            const error = new Error(`${typeof temp} ${temp} bukan sebuah angka`)
+            throw error
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {
+    celciusToKelvin
+}
